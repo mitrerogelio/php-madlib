f3e81e302fc2ff89ae67f9441abfcdc695f6f716
Rogelio Mitre <rogelinmitre@gmail.com>
2020-12-24 14:41:49 -0600

    change title

--- site.php
+++ site.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mad Lib Project</title>
+    <title>MadLib Project</title>
 </head>
 <body>
     <h1>Your Madlib:</h1>
